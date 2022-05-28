255d2896946d77d3604209a330c18d5379171470
CosmicOrder <Matrixxturn@yandex.ru>
2022-05-28 17:00:44 +0300

    refactor: update similar variables, add 'serialized' to the names

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -6,16 +6,16 @@ from django.shortcuts import render
 def passcard_info_view(request, passcode):
     passcard = Passcard.objects.get(passcode=passcode)
     visits = Visit.objects.filter(passcard=passcard)
-    this_passcard_visits = []
+    serialized_passcard_visits = []
     for visit in visits:
         entered_at = visit.entered_at
         duration = visit.format_duration()
         is_strange = visit.is_visit_long()
-        this_passcard_visits.append({'entered_at': entered_at,
+        serialized_passcard_visits.append({'entered_at': entered_at,
                                      'duration': duration,
                                      'is_strange': is_strange})
     context = {
         'passcard': passcard,
-        'this_passcard_visits': this_passcard_visits
+        'this_passcard_visits': serialized_passcard_visits
     }
     return render(request, 'passcard_info.html', context)
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -5,18 +5,18 @@ from django.shortcuts import render
 
 def storage_information_view(request):
     current_visitors = Visit.objects.filter(leaved_at=None)
-    non_closed_visits = []
+    serialized_non_closed_visits = []
     for current_visitor in current_visitors:
         who_entered = current_visitor.passcard.owner_name
         entered_at = current_visitor.entered_at
         duration = current_visitor.format_duration()
         is_strange = current_visitor.is_visit_long()
-        non_closed_visits.append({'who_entered': who_entered,
+        serialized_non_closed_visits.append({'who_entered': who_entered,
                                   'entered_at': entered_at,
                                   'duration': duration,
                                   'is_strange': is_strange})
 
     context = {
-        'non_closed_visits': non_closed_visits,
+        'non_closed_visits': serialized_non_closed_visits,
     }
     return render(request, 'storage_information.html', context)
