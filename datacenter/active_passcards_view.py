9644198ed6b40b9756606e26b686bd9cd4d81453
CosmicOrder <Matrixxturn@yandex.ru>
2022-05-28 15:42:18 +0300

    refactor: delete unused code

--- datacenter/active_passcards_view.py
+++ datacenter/active_passcards_view.py
@@ -6,7 +6,6 @@ from django.shortcuts import render
 def active_passcards_view(request):
     active_passcards = Passcard.objects.filter(is_active=True)
 
-    all_passcards = Passcard.objects.all()
     context = {
         'active_passcards': active_passcards,
     }
